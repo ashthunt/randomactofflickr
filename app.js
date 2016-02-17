de5fb2ca9fc44e76232167d9f4cb1d2a3c0c8d5d
ashtonhunter <huntera2@tcnj.edu>
2016-02-17 12:27:17 -0500

    removed global references

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 
-function setbg($size, $photoid, $farmid, $server, $secret){
+function setbg(size, photoid, farmid, server, secret){
 
 	// $img = "https://farm2.static.flickr.com/1596/24345077003_27615dbb16_c.jpg";
 	// $size = "c";
@@ -12,16 +12,16 @@ function setbg($size, $photoid, $farmid, $server, $secret){
 	// $secret = "ca7c134a3b";
 	// alert('got here');
 
-	$img =
-	 'https://farm'+$farmid+'.static.flickr.com/'+$server+'/'+$photoid+'_'+$secret+'_'+$size+'.jpg';
-	//  alert($img);
+	var img =
+	 'https://farm'+farmid+'.static.flickr.com/'+server+'/'+photoid+'_'+secret+'_'+size+'.jpg';
+
+	//  alert(img);
 	 $("#container").css('background-size', 'cover');
-	$("#container").css('background-image', 'url('+$img+')');
-
-
+	$("#container").css('background-image', 'url('+img+')');
 }
 
 function makerequest(){
+
 
 	$.get("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=6c831824ff9e6778397aa5d3b9ad6cda&format=json&username=bestofrallylive&nojsoncallback=1").done(function(response){
 
@@ -40,23 +40,24 @@ function printme(){
 }
 
 function buildstring($o){
-	var $str = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=6c831824ff9e6778397aa5d3b9ad6cda&user_id="+$o+"&format=json&per_page=200&nojsoncallback=1";
+	var str = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=6c831824ff9e6778397aa5d3b9ad6cda&user_id="+$o+"&format=json&per_page=200&nojsoncallback=1";
 	// alert(str);
-	request($str);
+	request(str);
 }
 
 function request(str){
 	// alert(str);
 	$.get(str).done(function(data){
 		// alert(data.photos.photo[0].id);
-		$index = 180;
-		$photoid = data.photos.photo[$index].id;
-		$farmid = data.photos.photo[$index].farm;
-		$server = data.photos.photo[$index].server;
-		$secret = data.photos.photo[$index].secret;
-				// alert($photoid);
+		var index = 180;
+		var photoid = data.photos.photo[index].id;
+		var farmid = data.photos.photo[index].farm;
+		var server = data.photos.photo[index].server;
+		var secret = data.photos.photo[index].secret;
+				// alert(photoid);
 
-				setbg("c", $photoid, $farmid, $server, $secret);
+				setbg("c", photoid, farmid, server, secret);
+
 	});
 
 }
